42fb192b013aa61971bbd8416f24184ea3618a2f
Keshav Lakshmi Narasimhan <keshavlakshminarasimhan@gmail.com>
2025-07-17 17:03:49 +0530

    Add files via upload

---
+++ leftrightcontrol.ino
@@ -0,0 +1,44 @@
+#include <ESP32Servo.h>
+#define LED_BUILTIN 2
+Servo pan;
+Servo tilt;
+Servo trigger;
+int pcurrent = 90;
+int tcurrent = 60;
+ void setup() {
+  Serial.begin(115200);
+  pan.attach(15);
+  pan.write(pcurrent);
+  tilt.attach(19);
+  tilt.write(tcurrent);
+  trigger.attach(18);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+    if (command == "LEFT" && pcurrent < 180) {
+      pan.write(pcurrent+1);
+      pcurrent++;
+    } else if (command == "RIGHT" && pcurrent > 0) {
+      pan.write(pcurrent - 1);
+      pcurrent--;
+    } else if (command == "UP" && tcurrent > 0) {
+      tilt.write(tcurrent - 1);
+      tcurrent--;
+    } else if (command == "DOWN" && tcurrent < 90) {
+      tilt.write(tcurrent + 1);
+      tcurrent++;
+    } else if (command == "SHOOT") {
+        trigger.write(60);
+        delay(300);
+        trigger.write(100);
+        delay(300);
+        trigger.write(90);
+        delay(500);
+    }
+    
+  }
+}
